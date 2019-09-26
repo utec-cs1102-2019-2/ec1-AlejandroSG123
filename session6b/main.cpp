866dd28a3a3b0c5883d16334ecc16a4ce232f70a
unknown <alejandro.surco.g@utec.edu.pe>
2019-09-26 09:48:17 -0500

    _session6b

---
+++ session6b/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 0;
+    cin>>n;
+    string *codigos = new string [n];
+    string *nombres = new string [n];
+    int *edades = new int [n];
+    char *sexo = new char [n];
+    
+    return 0;
+}
